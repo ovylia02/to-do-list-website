e5578d309d15a5cfbb2c0fb8ff613317f48ea8b3
Ovylia <ovylia02@gmail.com>
2025-05-14 13:10:01 +0700

    Update font

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['doneId'])) {
         <!--FONTS-->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
     
         <!--UI-->
         <link rel="stylesheet" href="style.css" />
